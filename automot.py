07f0e82474f335808f83dc3cfc90321add00eb17
TiagoCarvalho-true <carvalhost58@gmail.com>
2025-01-05 11:43:32 -0400

    pesquisei mais a fundo sobre a api da meta, que so tem disposição para feeds

--- automot.py
+++ automot.py
@@ -3,25 +3,29 @@ import datetime
 import requests
 
 # Configurações do Meta
-META_GRAPH_API_URL = 'https://graph.facebook.com/v13.0/{page_id}/media'
+INSTAGRAM_GRAPH_API_URL = 'https://graph.facebook.com/v13.0/{ig_user_id}/media'
+INSTAGRAM_PUBLISH_URL = 'https://graph.facebook.com/v13.0/{ig_user_id}/media_publish'
 ACCESS_TOKEN = 'token_de_acesso_aqui'
+IG_USER_ID = 'seu_usuario_instagram_id'
 
-# Função para fazer o upload de um post
-def upload_post(filename, post_date):
-    with open(filename, 'rb') as file:
-        files = {'source': file}
-        data = {
-            'caption': '',
-            'scheduled_publish_time': post_date.timestamp(),
-            'published': False,
-            'access_token': ACCESS_TOKEN
-        }
-        response = requests.post(META_GRAPH_API_URL.format(page_id='sua_pagina_id'), files=files, data=data)
-        
-        if response.status_code == 200:
-            print(f'Successfully uploaded {filename}')
-        else:
-            print(f'Failed to upload {filename}. Status code: {response.status_code} - {response.json()}')
+# Função para criar um container de mídia (para feed)
+def create_media_container(image_path, caption):
+    files = {'image_url': (None, image_path)}  # URL remota da imagem
+    data = {
+        'caption': caption,
+        'access_token': ACCESS_TOKEN,
+    }
+    response = requests.post(INSTAGRAM_GRAPH_API_URL.format(ig_user_id=IG_USER_ID), data=data, files=files)
+    return response.json()
+
+# Função para publicar o container de mídia
+def publish_media(media_id):
+    data = {
+        'creation_id': media_id,
+        'access_token': ACCESS_TOKEN
+    }
+    response = requests.post(INSTAGRAM_PUBLISH_URL.format(ig_user_id=IG_USER_ID), data=data)
+    return response.json()
 
 # Diretório com os posts
 directory = 'caminho_para_sua_pasta'
@@ -29,8 +33,8 @@ directory = 'caminho_para_sua_pasta'
 # Percorre todos os arquivos na pasta
 for filename in os.listdir(directory):
     if filename.endswith('.png') or filename.endswith('.jpg'):
-        # Extrai o dia do nome do arquivo
         try:
+            # Extrai o dia do nome do arquivo
             day = int(filename.split('_')[0])
         except ValueError:
             print(f'Nome de arquivo inválido: {filename}')
@@ -39,8 +43,19 @@ for filename in os.listdir(directory):
         # Configura a data de postagem para o dia especificado no nome do arquivo às 7 horas da manhã
         now = datetime.datetime.now()
         post_date = datetime.datetime(now.year, now.month, day, 7, 0, 0)
-        
-        # Chama a função para fazer o upload
-        upload_post(os.path.join(directory, filename), post_date)
+        if post_date < now:
+            print(f"Data já passada para {filename}, ignorando...")
+            continue
 
-print("All posts processed.")
+        # Cria o container de mídia
+        caption = f'Story programado para {post_date.strftime("%d/%m/%Y %H:%M")}'
+        response = create_media_container(os.path.join(directory, filename), caption)
+        
+        if 'id' in response:
+            media_id = response['id']
+            publish_response = publish_media(media_id)
+            print(f"Publicado como feed para {filename}: {publish_response}")
+        else:
+            print(f"Erro ao criar container para {filename}: {response}")
+
+print("Processamento concluído.")
